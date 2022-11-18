380849910578812c3c426d6404590fd7155d3d50
Julian Stradolini <julianstradolini@gmail.com>
2022-11-18 00:31:43 -0300

    adding components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ToolbarComponent } from './toolbar/toolbar/toolbar.component';
+import { GeneralComponent } from './mainbody/general/general.component';
+import { MenuComponent } from './mainbody/general/menu/menu.component';
+import { ContentAreaComponent } from './mainbody/general/content-area/content-area.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolbarComponent,
+    GeneralComponent,
+    MenuComponent,
+    ContentAreaComponent
   ],
   imports: [
     BrowserModule
